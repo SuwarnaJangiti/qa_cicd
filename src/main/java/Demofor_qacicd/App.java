63bf6ebd947400d253c9a0c6341bc1e9fab2d51c
suwarna <suwarna@naarsoft.com>
2023-12-19 16:34:46 +0530

    initial commit

---
+++ src/main/java/Demofor_qacicd/App.java
@@ -0,0 +1,13 @@
+package Demofor_qacicd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Demofor_qacicd/TestClass.java
@@ -0,0 +1,67 @@
+package Demofor_qacicd;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestClass {
+	WebDriver driver;
+	ExtentReports extentReport;
+	ExtentSparkReporter sparkReporter;
+	ExtentTest test;
+	@BeforeTest
+	public void initializingReports() {
+		extentReport=new ExtentReports();
+		sparkReporter=new ExtentSparkReporter("report.html");
+		extentReport.attachReporter(sparkReporter);
+	}
+	
+	@BeforeMethod
+	public void inintializingTest(ITestResult result) {
+		test=extentReport.createTest(result.getMethod().getMethodName());
+	}
+	
+	@Test
+	public void verifyTitle() {
+	//WebDriverManager.chromedriver().setup();
+	ChromeOptions options=new ChromeOptions();
+	options.addArguments("headless");
+	WebDriverManager.chromedriver().setup();
+	driver=new ChromeDriver(options);
+	driver.get("https://www.browserstack.com/");
+	System.out.println("Title is: " +driver.getTitle());
+	Assert.assertEquals(driver.getTitle(), "Most Reliable App & Cross Browser Testing Platform | BrowserStack");
+	driver.quit();
+	}
+	
+	@AfterMethod
+	public void aftermethod(ITestResult result) {
+		if(result.getStatus()==result.SUCCESS) {
+			test.pass(result.getMethod().getMethodName()+" : Passed");
+		}
+		else {
+			test.fail(result.getMethod().getMethodName()+" : Failed");
+			test.log(Status.INFO, result.getThrowable());
+		}
+	}
+	
+	@AfterTest
+	public void flushTheReport() {
+		extentReport.flush();
+	}
+
+}
